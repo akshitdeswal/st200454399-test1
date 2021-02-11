17f4be5eb92a8390f85a9db57c38c53bcd5b7046
akshit-deswal <72546148+akshit-deswal@users.noreply.github.com>
2021-02-11 12:33:20 -0500

    setName(), setType and setAge complete

--- src/Pet.java
+++ src/Pet.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Pet {
     private String name;
     private String type;
@@ -27,4 +30,25 @@ public class Pet {
         return color;
     }
 
+    public void setName(String name) {
+        if (name.length() >= 2)
+            this.name = name;
+        else
+            throw new IllegalArgumentException("name must contain at least 2 characters");
+    }
+
+    public void setType(String type) {
+        List<String> validTypes =Arrays.asList("dog", "cat", "bird", "reptile", "fish");
+        if(validTypes.contains(type))
+            this.type = type;
+        else
+            throw new IllegalArgumentException(type+ " is an invalid input, please choose from these options: "+validTypes);
+    }
+
+    public void setAge(int age) {
+        if(age<60 && age>0)
+            this.age = age;
+        else
+            throw new IllegalArgumentException(age+" is not a valid input, please select age between 0-60 years");
+    }
 }
